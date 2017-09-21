23140246312cadde7ba588e6ab04afd46600a194
Gerrit Niezen <gerrit.niezen@gmail.com>
2017-09-21 10:22:44 +0100

    switching to abbott and using await instead of then blocks

--- webusb.js
+++ webusb.js
@@ -2,43 +2,51 @@ document.addEventListener('DOMContentLoaded', event => {
   let button = document.getElementById('connect')
   let device
   button.addEventListener('click', async() => {
-    const VENDOR_ID = 0x1a79
-    navigator.usb.requestDevice({
-      filters: [{
-        vendorId: VENDOR_ID
-      }]
-    }).then(selectedDevice => {
-      device = selectedDevice
+    try {
+      const VENDOR_ID = 0x1A61
+      device = await navigator.usb.requestDevice({
+        filters: [{
+          vendorId: VENDOR_ID
+        }]
+      })
+
       console.log('open')
-      var tOpen = device.open()
-
+      await device.open()
       console.log('opened:', device)
-      return tOpen
-    }).then(() => {
+
       console.log('selectConfiguration')
-      return device.selectConfiguration(1)
-    }).then(() => {
+      await device.selectConfiguration(1)
+
       console.log('claimInterface')
-      return device.claimInterface(0)
-    }).then(() => {
-      console.log('controlTransferOut')
+      await device.claimInterface(0)
 
-      const data = new Uint8Array(64)
-      data.set([0x41, 0x42, 0x43, 0x01, 0x06])
+      console.log('transferOut')
 
-      return device.transferOut(0x01, data.buffer)
-    }).then(result => {
+      const data = new Uint8Array(10)
+      // data.set([0x6d,0x65,0x6d])
+
+      const result = await device.controlTransferOut({
+        requestType: 'vendor',
+        recipient: 'device',
+        request: 0x05,
+        value: 0x0000,
+        index: 0x0003
+      }, data)
+      // await device.transferOut(0x01, data.buffer)
+
       console.log(result)
 
-      device.transferIn(1, 64).then(result => {
-        console.log(result)
-        let decoder = new TextDecoder()
-        console.log('Received: ' + decoder.decode(result.data))
-      }, error => {
-        console.log('Error:', error)
-      })
-    }).catch(error => {
+        /*
+        device.transferIn(1, 14).then(result => {
+          console.log(result)
+          let decoder = new TextDecoder()
+          console.log('Received: ' + decoder.decode(result.data))
+        }, error => {
+          console.log('Error:', error)
+        })
+        */
+    } catch (error) {
       console.log(error)
-    })
+    }
   })
 })
